13f6576931107afede519bbe59fc31559dce7da9
Diego Velazquez <govelazmen@gmail.com>
2025-03-28 02:19:01 -0600

    Add: procesador de datos de humedad

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"log"
-	"Xilonen-2/sensorAire/messaging"
+	aire "Xilonen-2/sensorAire/messaging"
+	humedad "Xilonen-2/sensorHumedad/messaging"
 )
 
 func main() {
 	log.Println("🚀 Iniciando consumidor de datos...")
-	messaging.StartConsumer()
+	go aire.StartConsumer()
+	go humedad.StartHumedadConsumer()
+
+	select {}
 }
